5ee0e2205bff614bea214e4513491890fdde3f60
benhardtitathiono <113670466+benhardtitathiono@users.noreply.github.com>
2023-03-25 14:10:06 +0700

    init

---
+++ app/src/main/java/com/ubaya160420092/adv160420092week4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ubaya160420092.adv160420092week4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
