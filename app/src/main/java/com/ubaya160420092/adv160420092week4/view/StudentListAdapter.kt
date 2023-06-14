52df5900c1003ebfb6b27b1f020f0b34a0f2adc2
blixondi <blixondi@gmail.com>
2023-06-14 13:58:25 +0700

    convert to data binding layout

--- app/src/main/java/com/ubaya160420092/adv160420092week4/view/StudentListAdapter.kt
+++ app/src/main/java/com/ubaya160420092/adv160420092week4/view/StudentListAdapter.kt
@@ -7,41 +7,44 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.ubaya160420092.adv160420092week4.R
+import com.ubaya160420092.adv160420092week4.databinding.StudentListItemBinding
 import com.ubaya160420092.adv160420092week4.model.Student
 import com.ubaya160420092.adv160420092week4.util.loadImage
 
 class StudentListAdapter(val studenList:ArrayList<Student>): RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>() {
 
-    class StudentViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+    class StudentViewHolder(var view: StudentListItemBinding) : RecyclerView.ViewHolder(view.root)
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): StudentListAdapter.StudentViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.student_list_item, parent, false)
-
+//        val view = inflater.inflate(R.layout.student_list_item, parent, false)
+        val view = DataBindingUtil.inflate<StudentListItemBinding>(inflater, R.layout.student_list_item,
+            parent, false)
         return StudentViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: StudentListAdapter.StudentViewHolder, position: Int) {
-        val txtID = holder.view.findViewById<TextView>(R.id.txtID)
-        val txtName = holder.view.findViewById<TextView>(R.id.txtName)
-        val btnDetail = holder.view.findViewById<Button>(R.id.btnDetail)
-        txtID.text = studenList[position].id
-        txtName.text = studenList[position].name
-        btnDetail.setOnClickListener{
-            val student_id=txtID.text.toString()
-            val action = StudentListFragmentDirections.actionStudentDetail(student_id)
-            Navigation.findNavController(it).navigate(action)
-        }
-
-        var imageView = holder.view.findViewById<ImageView>(R.id.imageView)
-        var progressBar = holder.view.findViewById<ProgressBar>(R.id.progressBar)
-        imageView.loadImage(studenList[position].photoUrl, progressBar)
+//        val txtID = holder.view.findViewById<TextView>(R.id.txtID)
+//        val txtName = holder.view.findViewById<TextView>(R.id.txtName)
+//        val btnDetail = holder.view.findViewById<Button>(R.id.btnDetail)
+//        txtID.text = studenList[position].id
+//        txtName.text = studenList[position].name
+//        btnDetail.setOnClickListener{
+//            val student_id=txtID.text.toString()
+//            val action = StudentListFragmentDirections.actionStudentDetail(student_id)
+//            Navigation.findNavController(it).navigate(action)
+//        }
+//
+//        var imageView = holder.view.findViewById<ImageView>(R.id.imageView)
+//        var progressBar = holder.view.findViewById<ProgressBar>(R.id.progressBar)
+//        imageView.loadImage(studenList[position].photoUrl, progressBar)
 
     }
 
